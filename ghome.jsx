0d4d90751ef3a35056dbded525bc43ad8f3af999
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2023-11-13 10:21:20 +0530

    Add files via upload

---
+++ ghome.jsx
@@ -0,0 +1,87 @@
+
+import React from 'react';
+import Back from '/image/back.jpg';
+
+
+
+
+
+
+
+
+
+function ghome(){
+
+
+ 
+
+ 
+ 
+   
+      
+        
+    return(
+      
+     <div >
+
+<img src={Back}  style={{width: '1550px',height:'700px' , top: '60px', position: 'absolute'}}/>
+   
+   <div>
+   <herader>
+
+    <div style={{display: 'flex', top: '20px', position:'absolute'}}>
+    <div style={{left:'100px', position: 'absolute', fontFamily: 'impact', fontSize:'20px', color:'#413c39'}}>zon.net</div>
+    <div style={{position:'absolute',left: '1000px'}}>
+    <div style={{position:'absolute',left: '100px',fontFamily:'initial', color:'#4d4d4d'}}>Home</div>
+    <div style={{position:'absolute',left: '200px',fontFamily:'initial', color:'#4d4d4d'}}> More</div>
+    <div style={{position:'absolute',left: '300px',fontFamily:'initial', color:'#4d4d4d'}}>About</div>
+    <div style={{position:'absolute',left: '400px',fontFamily:'initial', color:'#4d4d4d'}}>Contact</div>
+    </div>
+    </div>
+
+
+     <div style={{position:'absolute',width:'500px'}}>
+    <div style={{left:'150px', position: 'absolute', top: '130px', fontFamily:'impact',fontSize:'50px', color:'#b7bbc0'}}>Create Your Website</div>
+    </div>
+    <div style={{left:'80px', position: 'absolute', width: '500px'}}>
+    <div style={{left:'80px', position: 'absolute', top: '300px',fontFamily:'initial', color:'#1b3833'}}>After trying these steps, attempt to run your npm run dev command again. If the issue persists, 
+      there might be a specific configuration or conflict on your system, and it may be helpful to consult 
+      with your IT support or seek assistance on forums related to PowerShell and npm interactions.</div>
+      </div>
+    <button style={{left:'160px', 
+                    position: 'absolute', 
+                    top: '490px', 
+                    border: 'none',
+                    borderRadius:'50px', 
+                    backgroundColor:'#ab7b79',
+                    color:'white',
+                    fontFamily:'initial',
+                    width:'120px',
+                    height:'50px'
+                    }}>READ MORE</button>
+
+
+   </herader>
+
+   </div>
+
+      
+           
+
+   </div>
+
+
+     
+        
+      
+
+
+
+                     
+                   
+
+  
+      
+    )
+    }
+export default ghome;
